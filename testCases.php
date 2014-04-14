054aa1e405a49f8a01ff8236d7e5a1108b6291ee
Daniel Toll <daniel@familjentoll.se>
2014-04-14 11:11:17 +0200

    Update testCases.php

--- testCases.php
+++ testCases.php
@@ -8,17 +8,17 @@ function tryArray(array $inputArray, $expectedOutput, $expectedException = false
 			$actual = mymax($inputArray[0],$inputArray[1],$inputArray[2]);
 			if ($expectedException == false) {
 				if ($actual != $expectedOutput) {
-					echo ("wrong output [$actual] for [$inputArray[0],$inputArray[1],$inputArray[2]]should be [$expectedOutput] </br>");
+					throw new \Exception ("wrong output [$actual] for [$inputArray[0],$inputArray[1],$inputArray[2]]should be [$expectedOutput] </br>");
 					$errors++;
 				}
 			} else {
-				echo("wrong output [$actual] for [$inputArray[0],$inputArray[1],$inputArray[2]]should be [Exception]  </br>");
+				throw new \Exception ("wrong output [$actual] for [$inputArray[0],$inputArray[1],$inputArray[2]]should be [Exception]  </br>");
 				$errors++;
 			}
 		} catch (\Exception $e) { 
 			
 			if ($expectedException == false) {
-				echo("wrong output [Exception] for [$inputArray[0],$inputArray[1],$inputArray[2]]should be [$expectedOutput] </br>");
+				throw new \Exception ("wrong output [Exception] for [$inputArray[0],$inputArray[1],$inputArray[2]]should be [$expectedOutput] </br>");
 				$errors++;
 			} else {
 				//OK
@@ -47,7 +47,7 @@ function test() {
 	tryArray($stringInput, 0, true);
 	tryArray($arrayInput, 0, true);
 
-	echo "true";	
+	throw new \Exception("ok");	
 }
 
 test();
