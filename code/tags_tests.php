d9c5d2252818d60d0032b7914d07b29b834c3a73
Daniel Toll <daniel@familjentoll.se>
2014-04-14 13:54:13 +0200

    daniel testar

--- code/tags_tests.php
+++ code/tags_tests.php
@@ -5,3 +5,6 @@
 if (isset($codeOutput ) == false) {
 	throw new \Exception("no output from program should output Hello World");
 }
+
+if ($codeOutput != "Hello World")
+	throw new \Exception("output was [$codeOutput] from program should output [Hello World]");
