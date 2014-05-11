da502986d00841f542a518ceefe35d69503c4775
Daniel Toll <daniel@familjentoll.se>
2014-05-11 13:32:07 +0200

    refine

--- code/instance_tests.php
+++ code/instance_tests.php
@@ -1,6 +1,8 @@
 <?php
 
-/*
+ob_start();
+ob_implicit_flush(false); 
+
 $pl = new \model\ProductList();
 $pl->add(new \model\Product("Banana", 1.5));
 $pl->add(new \model\Product("Orange", 2.5));
@@ -8,23 +10,25 @@ $pl->add(new \model\Product("Apple", 3.5));
 
 $pv = new \view\ProductView($pl);
 
-$pv->show();*/
+$pv->show();
+$expectedOutput = ob_get_contents();
+ob_clean();
 
-$expectedOutput = "<h3>A List of products</h3>
+/*$expectedOutput = "<h3>A List of products</h3>
 <p>Number of products 3 </p>
 <ul>
 	<li>Product: <strong>Banana</strong> Price: 1.5 sek</li>
 	<li>Product: <strong>Orange</strong> Price: 2.5 sek</li>
 	<li>Product: <strong>Apple</strong> Price: 3.5 sek</li>
 </ul>
-<p>Total Price: 7.5sek</p>";
+<p>Total Price: 7.5sek</p>";*/
 
 if (isset($codeOutput) == false) {
 	throw new \Exception("no output from program should output $expectedOutput");
 }
 
 if (compareOutput($codeOutput,$expectedOutput) == false) {
-	var_dump($codeOutput);
-	var_dump($expectedOutput);
+	//var_dump($codeOutput);
+	//var_dump($expectedOutput);
 	throw new \Exception("output was [$codeOutput] from program should output [$expectedOutput]");
 }
