e76f571712b90a12dfdaadc2f94642772d711151
Daniel Toll <daniel@familjentoll.se>
2014-05-11 11:22:31 +0200

    refine

--- code/instance_tests.php
+++ code/instance_tests.php
@@ -1,6 +1,16 @@
 <?php
 
-$expectedOutput = "<h3>A List of products</h3><p>Number of products 3 </p><ul><li>Banana 1.5 sek </li><li>Orange 2.5 sek </li><li>Apple 3.5 sek </li></ul><p>Total Price: 7.5sek</p>";
+/*
+$pl = new \model\ProductList();
+$pl->add(new \model\Product("Banana", 1.5));
+$pl->add(new \model\Product("Orange", 2.5));
+$pl->add(new \model\Product("Apple", 3.5));
+
+$pv = new \view\ProductView($pl);
+
+$pv->show();*/
+
+$expectedOutput = "<h3>A List of products</h3>\n<p>Number of products 3 </p>\n<ul>\n\t<li>Banana 1.5 sek </li>\n\t<li>Orange 2.5 sek </li>\n\t<li>Apple 3.5 sek </li>\n</ul>\n<p>Total Price: 7.5sek</p>";
 
 if (isset($codeOutput) == false) {
 	throw new \Exception("no output from program should output $expectedOutput");
