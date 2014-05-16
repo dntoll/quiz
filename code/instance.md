30d7c1922bcf4f9d95bcf02480c338c5a5d5c935
Daniel Toll <daniel@familjentoll.se>
2014-05-16 14:38:34 +0200

    more comments

--- code/instance.md
+++ code/instance.md
@@ -3,9 +3,9 @@
 ## Code
 	An object is constructed by using the new operator on a class. 
 	A class in a namespace needs to be adressed by its namespace
-	For example 
+	For example:
 	$db = new \model\Database("root", "password");
-	Task: Use the defined classes to show a list of products.
+	Task: Use the defined classes to echo a list of products to the output buffer
 E http://127.0.0.1:8888/validator.php
 V https://raw.github.com/dntoll/quiz/master/code/instance_tests.php
 A https://raw.github.com/dntoll/quiz/master/code/instance_default.php
--- code/instance_default.php
+++ code/instance_default.php
@@ -10,7 +10,6 @@ Price List:
 Banana 1.5 sek 
 Orange 2.5 sek
 Apple 3.5 sek 
-
 */
 
 //Write your code here
@@ -20,27 +19,56 @@ Apple 3.5 sek
 
 namespace model;
 
-class Product {
-	private $title;
-	private $price;
 
-	public function __construct($title, $price) {
+/**
+* Class model\Product represents a product with price
+*/
+class Product {
+	/**
+	* @var String title, the product name
+	*/
+	private $title;
+
+	/**
+	* @var float price, the price in SEK
+	*/
+	private $priceSEK;
+
+	/**
+	* Constructs a Product object
+	* 
+	* @throws \Exception if input is wrong
+	*
+	* @param String $title, the product title, length must be > 0 characters
+	* @param float $priceSEK, the price in SEK, must be larger than 0 SEK
+	*/
+	public function __construct($title, $priceSEK) {
 		if (is_string($title) === FALSE || strlen($title) < 1)
 			throw new \Exception("Product title must be of string type with length > 0");
 
-		if (is_numeric($price) === FALSE || floatval($price) < 0)
+		if (is_numeric($priceSEK) === FALSE || floatval($priceSEK) < 0)
 			throw new \Excpetion("Product price must be numeric type and larger than 0 ");
 
 		$this->title = $title;
-		$this->price = $price;
+		$this->priceSEK = $priceSEK;
 	}
 
+	/**
+	* get product title
+	*
+	* @return String title, the product name
+	*/
 	public function getTitle() {
 		return $this->title;
 	}
 
+	/**
+	* get Price in SEK
+	*
+	* @return float price, the price in SEK
+	*/
 	public function getPrice() {
-		return $this->price;
+		return $this->priceSEK;
 	}
 }
 
@@ -49,26 +77,58 @@ class Product {
 
 namespace model;
 
+
+/**
+* A List of Products 
+*
+*/
 class ProductList implements \IteratorAggregate {
 
+	/**
+	 * @var array of \model\Product
+	 */
 	private $products;
 
+	/**
+	 * Constructor
+	 */
 	public function __construct() {
 		$this->products = array();
 	}
 
+	/**
+	 * Adds an product to the list
+	 * 
+	 * @param Product $toBeAdded
+	 */
 	public function add(Product $toBeAdded) {
 		$this->products[] = $toBeAdded;
 	}
 
+	/**
+	 * Returns the number of products
+	 * 
+	 * @return integer, the number of products added to the list
+	 */
 	public function getCount() {
 		return count($this->products);
 	}
 
+	/**
+	 * returns an ArrayIterator object so that the ProductList can be used in an foreach loop
+	 * as an array
+	 * 
+	 * @return \ArrayIterator
+	 */
 	public function getIterator() {
         return new \ArrayIterator($this->products);
     }
 
+    /**
+     * returns the total price of all products added to the list
+     * 
+     * @return float, the total price
+     */
     public function getTotalPrice() {
     	$total = 0;
 		foreach ($this->products as $product) {
@@ -82,16 +142,35 @@ class ProductList implements \IteratorAggregate {
 
 namespace view;
 
+
+/**
+ * The ProductView class creates HTML output from an \model\ProductList 
+ */
 class ProductView {
 
+	/**
+	 * The product list object to create HTML from
+	 * @var \model\ProductList
+	 */
 	private $productList;
 
+	/**
+	 * Constructor creates an ProductView object
+	 * 
+	 * @param \model\ProductList $products
+	 */
 	public function __construct(\model\ProductList $products) {
 		$this->productList = $products;
 	}
 
+	/**
+	 * Outputs HTML-representation of all products in the $productList
+	 *
+	 * Note that this echoes the output to the output buffer
+	 * 
+	 * @return void
+	 */
 	public function show() {
-
 		$count = $this->productList->getCount();
 		$totalPrice = $this->productList->getTotalPrice();
 
@@ -107,6 +186,14 @@ class ProductView {
 		echo "<p>Total Price: $totalPrice sek</p>";
 	}
 
+	/**
+	 * Internal show products method
+	 * 
+	 * Note that this echoes the output to the output buffer
+	 * 
+	 * @param  \model\Product $toShow
+	 * @return void
+	 */
 	private function showProduct(\model\Product $toShow) {
 
 		$title = $toShow->getTitle();
