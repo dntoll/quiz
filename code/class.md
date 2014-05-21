9d613fd41c4b35871ed0dce5eb9a973fa30e7a92
Daniel Toll <daniel@familjentoll.se>
2014-05-21 16:10:02 +0200

    new task

---
+++ code/class.md
@@ -0,0 +1,11 @@
+# PHP Classes
+
+## Code
+	Use the defined class view to output a HTML page : 
+E http://127.0.0.1:8888/validator.php
+V https://raw.github.com/dntoll/quiz/master/code/class_tests.php
+A https://raw.github.com/dntoll/quiz/master/code/class_default.php
+
+## Good Work
+	Return to the list of tasks
+U http://csquiz.lnu.se:88/student.php?uid=
---
+++ code/class_default.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$view = new HTMLPageView("foo", "boody");
+
+$view->echoHTML();
+
+[FILEBREAK]<?php
+
+class HTMLPageView {
+
+	private $title;
+	private $bodyHTML;
+
+	public function __construct($title, $body) {
+		$this->title = $title;
+		$this->bodyHTML = $body;
+	}
+
+	public function echoHTML() {
+		echo "
+		<!DOCTYPE html>
+		<html>
+			<head>
+			<meta charset="utf-8">
+			<title>$this->title</title>
+			</head>
+			<body>
+				$this->bodyHTML
+			</body>
+		</html>";
+
+	}
+}
